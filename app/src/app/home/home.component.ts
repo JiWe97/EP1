5e27e129bfe9f895c6917d30125df3c755e5266a
Jill Wets <wetsjill@gmail.com>
2024-01-11 23:16:52 +0100

    Displaying API content in a list of cards

--- app/src/app/home/home.component.ts
+++ app/src/app/home/home.component.ts
@@ -9,18 +9,20 @@ import { FormsModule } from '@angular/forms';
   imports: [CommonModule, RouterOutlet, FormsModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
-  
+
 })
 export class HomeComponent {
-  title: string = 'Eindproef Frontend';
+  /* title: string = 'Eindproef Frontend'; */
   creators:string = 'Jill Wets (Scrum master), Shelsy De Ryck & Vicki Ramsdonck';
   url: string = 'https://api.spoonacular.com/recipes/complexSearch?';
   recipes: any [] = []; 
   search: any; 
   results: any;
+  title: any;
   mySearch: string = '';
   apiKey: string = 'apiKey=a1bb1c31a31948c8b57d41dd27e57ee8&query=';
   apiHost: string = 'spoonacular-recipe-food-nutrition-v1.p.rapidapi.com';
+  i: any;
   
 
   getData() {
@@ -38,7 +40,28 @@ export class HomeComponent {
       .catch(err => console.error(err));
   }
 
+  postData() {
+    for (let i = 0; i < 5; i++) {
+      const card = document.createElement("div");
+                  card.innerHTML = `
+                      <div class="card" style="width: 18rem;" >
+                        <img src="{{this.search[i].image}}" class="card-img-top" alt="{{this.search[i].title}}">
+                        <div class="card-body">
+                          <h5 class="card-title">{{this.search[i].title}}</h5>
+                          <a href="#" class="btn btn-primary">Let's cook!</a>
+                        </div>
+                      </div>
+                  `;
+    }
+  
+  }
 
+
+/* ngOnInit() {
+  for (let i = 0; i < 5; i++)
+  this.getData();
+  this.postData();
+} */
 }
 
 
