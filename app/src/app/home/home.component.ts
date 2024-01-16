6719682b2995c0cb7533598da55251b310312ccf
Jill Wets <wetsjill@gmail.com>
2024-01-16 11:27:25 +0100

    API call to get recipeinformation

--- app/src/app/home/home.component.ts
+++ app/src/app/home/home.component.ts
@@ -16,13 +16,17 @@ export class HomeComponent {
   creators:string = 'Jill Wets (Scrum master), Shelsy De Ryck & Vicki Ramsdonck';
   url: string = 'https://api.spoonacular.com/recipes/complexSearch?';
   recipes: any [] = []; 
+  results: any [] = [];
+  dish: any;
   search: any; 
-  results: any;
   title: any;
+  i: any;
+  id: any;
+  element: any;
   mySearch: string = '';
+  hideRecipeInformation: boolean = true;
   apiKey: string = 'apiKey=a1bb1c31a31948c8b57d41dd27e57ee8&query=';
   apiHost: string = 'spoonacular-recipe-food-nutrition-v1.p.rapidapi.com';
-  i: any;
   
 
   getData() {
@@ -32,30 +36,24 @@ export class HomeComponent {
           this.recipes = json.results;
           this.search = this.recipes.filter(recipes => recipes.title.includes(this.mySearch));
         if (this.search) {
-          console.log('Recipe:', this.search);
+          console.log('Recipes:', this.search);
         } else {
           console.log(`No recipes found for ${this.search}`);
         } 
       })
       .catch(err => console.error(err));
   }
-/* 
-  postData() {
-    for (let i = 0; i < 5; i++) {
-      const card = document.createElement("div");
-                  card.innerHTML = `
-                      <div class="card" style="width: 18rem;" >
-                        <img src="{{this.search[i].image}}" class="card-img-top" alt="{{this.search[i].title}}">
-                        <div class="card-body">
-                          <h5 class="card-title">{{this.search[i].title}}</h5>
-                          <a href="#" class="btn btn-primary">Let's cook!</a>
-                        </div>
-                      </div>
-                  `;
-    }
-  
-  } */
 
+  getRecipe(id: any) {
+    this.hideRecipeInformation = false;
+    const options = {method: 'GET', headers: {'User-Agent': 'insomnia/8.4.5'}};
+    fetch('https://api.spoonacular.com/recipes/' + id + '/information?' + this.apiKey, options)
+    .then(response => response.json())
+    .then(json => {
+      this.results = json;
+    })
+    .catch(err => console.error(err));
+  }
 
 ngOnInit() {
   this.getData();
