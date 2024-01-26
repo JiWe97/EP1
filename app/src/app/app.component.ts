05b607c6719c8406a360343e93eadff734e8d1de
Jill Wets <wetsjill@gmail.com>
2024-01-26 10:31:08 +0100

    Small css changes, added login to title

--- app/src/app/app.component.ts
+++ app/src/app/app.component.ts
@@ -13,24 +13,5 @@ import { ReactiveFormsModule } from '@angular/forms';
   
 })
 export class AppComponent {
-  isLoggedIn() {
-    const token = localStorage.getItem('token');
-    return token !== null; // Return true if a token is found
-  }
-
-  getUsername() {
-    // Implement logic to get the username of the logged-in user
-    try {
-      const userName = localStorage.getItem('userName');
-      if (userName) {
-        return userName;
-      } else {
-        // Handle case where userName is not set
-        return 'Unknown User'; // Placeholder, update as needed
-      }
-    } catch (e) {
-      console.error('Error accessing local storage:', e);
-      return 'Unknown User'; // Placeholder, handle this as per your application's logic
-    }
-  }
+  
 }
