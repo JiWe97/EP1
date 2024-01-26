a1961c4732a96495f329c463f42576cae0fdeef7
Jill Wets <wetsjill@gmail.com>
2024-01-26 21:35:25 +0100

    Code notation cleanup, changed overview of my recipes

--- app/src/app/my-recipe/my-recipe.component.ts
+++ app/src/app/my-recipe/my-recipe.component.ts
@@ -11,48 +11,92 @@ import { ToastrService } from 'ngx-toastr';
   styleUrl: './my-recipe.component.css'
 })
 export class MyRecipeComponent {
-url: string = 'http://localhost:8000/api/recipes';
-title: string = '';
-ingredient: string = '';
-step: string = '';
-userId: string = '';
-recipes: any[] = [];
-fieldsArray = ['', ''];
-recipe: any;
-mySearch: string = '';
-search: any;
-/* hideSearchInformation: boolean = true;
-hideRecipeInformation: boolean = false; */
+  url: string = 'http://localhost:8000/api/recipes';
+  title: string = '';
+  ingredient: string = '';
+  step: string = '';
+  userId: string = '';
+  recipes: any[] = [];
+  fieldsArray = ['', ''];
+  recipe: any;
+  mySearch: string = '';
+  search: any;
+  showSearchInformation: boolean = true;
+  showAddRecipe: boolean = false;
 
+  constructor(private toastr: ToastrService) { }
 
-constructor(private toastr: ToastrService) { }
-
-getToken() {
-  const token = localStorage.getItem('token');
-  if (!token) {
-    console.log('No token found in local storage');
-    return;
-  } else {
-    console.log('Token found in local storage:', token);
+  // Retrieve user_id from local storage
+  getToken() {
+    const token = localStorage.getItem('token');
+    if (!token) {
+      console.log('No token found in local storage');
+      return;
+    } else {
+      console.log('Token found in local storage:', token);
+    }
   }
-}
 
+  // Show input fields to add recipe
+  plus() {
+    this.showSearchInformation = false;
+    this.showAddRecipe = true;
+  }
+
+  // Go back to overview of favorites
+  goBack() {
+    this.showSearchInformation = true;
+    this.showAddRecipe = false;
+  }
+
+  // Displaying added recipes
+  postRecipe() {
+    const token = localStorage.getItem('token');
+    fetch(this.url)
+      .then(response => response.json())
+      .then(json => {
+        this.recipes = json;
+        this.recipe = this.recipes.filter(recipe => recipe.user_id === Number(token));
+        if (this.recipe) {
+          console.log('Recipes:', this.recipe);
+        } else {
+          console.log('No recipes found');
+        }
+      })
+      .catch(err => console.error(err));
+  }
+
+  deleteRecipe (id: any) {
+    const token = localStorage.getItem('token');
+    const options = {
+      method: 'DELETE',
+      headers: { 'Content-Type': 'application/json', 'User-Agent': 'insomnia/8.6.0' },
+    };
+    fetch(this.url + '/' + id, options)
+      .then(response => response.json())
+      .then(json => {
+        console.log(json);
+        this.postRecipe();
+      })
+      .catch(err => console.error(err));
+  }
+  // Add recipe to database
   submit() {
     this.toastr.success('Berry nice, you have added this recipe to your collection.', '', {
     })
     const token = localStorage.getItem('token');
     const options = {
       method: 'POST',
-      headers: {'Content-Type': 'application/json', 'User-Agent': 'insomnia/8.6.0'},
+      headers: { 'Content-Type': 'application/json', 'User-Agent': 'insomnia/8.6.0' },
       body: JSON.stringify({
-          "title": this.title,
-          "ingredient": this.ingredient,
-          "step": this.step,
-          "user_id": token
-    })
-     };
-     console.log('Options before fetch: '+JSON.stringify(options));
-      fetch(this.url, options)
+        "title": this.title,
+        "ingredient": this.ingredient,
+        "step": this.step,
+        "user_id": token
+      })
+    };
+    console.log('Options before fetch: ' + JSON.stringify(options));
+    fetch(this.url, options)
       .then(response => response.json())
       .then(response => {
         console.log(response);
@@ -61,18 +105,19 @@ getToken() {
         this.step;
       })
       .catch(err => console.error(err));
-  } 
-
-  addRecipe() {
-    
+    // Clear the fields
+    this.title = '';
+    this.ingredient = '';
+    this.step = '';
   }
 
+  // Search through recipes in API based on title
   searchRecipe() {
     fetch(this.url)
       .then(response => response.json())
       .then(json => {
-          this.recipes = json;
-          this.search = this.recipes.filter(recipes => recipes.title.includes(this.mySearch));
+        this.recipes = json;
+        this.search = this.recipes.filter(recipes => recipes.title.includes(this.mySearch));
         if (this.search) {
           console.log('Recipes:', this.search);
         } else {
@@ -80,23 +125,6 @@ getToken() {
         }
       })
       .catch(err => console.error(err));
-  }
-
-  // Displaying added recipes
-  postRecipe() {
-    const token = localStorage.getItem('token');
-    fetch(this.url)
-    .then(response => response.json())
-    .then(json => {
-        this.recipes = json;
-        this.recipe = this.recipes.filter(recipe => recipe.user_id === Number(token));
-      if (this.recipe) {
-        console.log('Recipes:', this.recipe);
-      } else {
-        console.log('No recipes found');
-      }
-    })
-    .catch(err => console.error(err));
   }
 
   // Display recipe information
@@ -108,21 +136,21 @@ getToken() {
 
 
 
-/* addField() {
-    // Add a new field
-    this.fieldsArray.push('');
-  }
-
-  submitForm() {
-    // making use of the destructor again ;) yay
-    const values = [...this.fieldsArray];
-    console.log(values);
-
-    // clear the fields;
-    this.fieldsArray = ['', ''];
-
-    alert(values.join('\n'));
-  } */
+  /* addField() {
+      // Add a new field
+      this.fieldsArray.push('');
+    }
+  
+    submitForm() {
+      // making use of the destructor again ;) yay
+      const values = [...this.fieldsArray];
+      console.log(values);
+  
+      // clear the fields;
+      this.fieldsArray = ['', ''];
+  
+      alert(values.join('\n'));
+    } */
 
   ngOnInit() {
     this.postRecipe();
