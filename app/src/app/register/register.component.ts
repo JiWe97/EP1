ea986fe1e9d9fa2a622fd6760b25d5beae49988f
vickiramsdonck <vicki.ramsdonck@student.pxl.be>
2024-01-28 22:57:17 +0100

    added comments and removed unused code

--- app/src/app/register/register.component.ts
+++ app/src/app/register/register.component.ts
@@ -12,6 +12,7 @@ import { ToastrService } from 'ngx-toastr';
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
+  //Declarations for user registration
   firstName:string ='';
   lastName:string='';
   email:string='';
@@ -19,10 +20,14 @@ export class RegisterComponent {
   userName:string='';
   showPassword: boolean = false;
 
+  //constructor with dependency injection
   constructor(private userService: UserService, private router: Router, private toastr: ToastrService) {}
 
+  //method to handle user submission
   onSubmit() {
+    //redirect to login
     this.router.navigate(['/login']);
+    //Create request options for POST request
     const options = {
       method: 'POST',
       headers: {'Content-Type': 'application/json', 'User-Agent': 'insomnia/8.6.0'},
@@ -34,12 +39,13 @@ export class RegisterComponent {
          "username": this.userName
       })
      };
-    
+    // Send POST request
     fetch('http://localhost:8000/api/users', options)
       .then(response => response.json())
       .then(response => console.log(response))
       .catch(err => console.error(err));
-      
+
+    //Log user registration into console  
     console.log('You have been registered:', {
       firstName: this.firstName,
       lastName: this.lastName,
@@ -48,14 +54,15 @@ export class RegisterComponent {
       userName: this.userName
     });
 
-    // acces the service and send username and password
+    //Use the userService to register the user
     this.userService.register(this.userName, this.password, this.firstName, this.lastName, this.email);// Show a notification
     this.toastr.success('Egg-celent, you have been registered!', '', {
       positionClass: 'toast-bottom-right'
     });
+    //log success into console
     console.log('You have been registered'); // Log to console
   
-      // clear the fields;
+    // clear the fields;
       this.userName = '';
       this.password = '';
       this.firstName = '';
@@ -63,7 +70,7 @@ export class RegisterComponent {
       this.email = '';
       
   }
-
+  // toggle password visibility
   togglePasswordVisibility(): void {
     this.showPassword = !this.showPassword;
   }
